4a786a9ff2c97f953a6fb2066cb695910611b07a
lenaber6 <lenaber6@mail.ru>
2024-02-01 23:23:28 +0300

    hw2react

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,38 @@ import EditTaskPage from "./components/EditTaskPage/EditTaskPage";
 import UserWindowPage from "./components/UserWindowPage/UserWindowPage";
 import MainContent from "./components/MainContent/MainContent";
 import Column from "./components/Column/Column";
+import { cardList } from "./data";
+import {useState, useEffect} from 'react'
+
+
+const statusList = [
+  "Без статуса",
+  "Нужно сделать",
+  "В работе",
+  "Тестирование",
+  "Готово",
+];
 
 function App() {
+  const [cards, setCards] = useState(cardList);
+  // eslint-disable-next-line no-undef
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); // 2 секунды задержки
+  }, []);
+  function addCard() {
+    // Логика добавления карточки
+    const newCard = {
+      id: cards.length +1,
+      theme: "Web Design",
+      title: "Название задачи",
+      date: "30.10.23",
+      status: "Без статуса",
+    }
+    setCards([...cards, newCard]);
+  }
   return (
     <div className="wrapper">
       {/* pop-up start*/}
@@ -17,14 +47,23 @@ function App() {
 
       {/* pop-up end*/}
 
-      <UserWindowPage />
-      <MainContent>
-        <Column title={"Без статуса"} />
+      <UserWindowPage addCard={addCard}/>
+      {isLoading ? "Загрузка..." : (<MainContent>
+        {statusList.map((status) => (
+          <Column
+            title={status}
+            key={status}
+            // eslint-disable-next-line no-undef
+            cardList={cards.filter((card) => card.status === status)}
+          />
+        ))}
+        {/*<Column title={"Без статуса"} />
         <Column title={"Нужно сделать"} />
         <Column title={"В работе"} />
         <Column title={"Тестирование"} />
-        <Column title={"Готово"} />
-      </MainContent>
+  <Column title={"Готово"} />*/}
+      </MainContent>)}
+      
     </div>
   );
 }
